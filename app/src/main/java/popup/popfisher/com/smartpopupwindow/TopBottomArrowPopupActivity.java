8d9d1455e23a01ce80ba1518d3be6b6c8a30db2b
yuzhangsheng <774232122@qq.com>
2016-10-09 22:36:01 +0800

    代码优化，加入返回键逻辑控制（在Activity中监听返回键）

--- app/src/main/java/popup/popfisher/com/smartpopupwindow/TopBottomArrowPopupActivity.java
+++ app/src/main/java/popup/popfisher/com/smartpopupwindow/TopBottomArrowPopupActivity.java
@@ -2,20 +2,13 @@ package popup.popfisher.com.smartpopupwindow;
 
 import android.app.Activity;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.widget.BaseAdapter;
-import android.widget.FrameLayout;
-import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class TopBottomArrowPopupActivity extends Activity implements View.OnClickListener {
@@ -26,6 +19,7 @@ public class TopBottomArrowPopupActivity extends Activity implements View.OnClic
     private View mButton4;
     private View mButton5;
     private View mButton6;
+    private PopupWindow mCurPopupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +44,7 @@ public class TopBottomArrowPopupActivity extends Activity implements View.OnClic
         int id = v.getId();
         switch (id) {
             case R.id.buttion1:
-                showTipPopupWindow(mButton1, new View.OnClickListener() {
+                mCurPopupWindow = showTipPopupWindow(mButton1, new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(getBaseContext(), "点击到弹窗内容", Toast.LENGTH_SHORT).show();
@@ -58,7 +52,7 @@ public class TopBottomArrowPopupActivity extends Activity implements View.OnClic
                 });
                 break;
             case R.id.buttion2:
-                showTipPopupWindow(mButton2, new View.OnClickListener() {
+                mCurPopupWindow = showTipPopupWindow(mButton2, new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(getBaseContext(), "点击到弹窗内容", Toast.LENGTH_SHORT).show();
@@ -66,7 +60,7 @@ public class TopBottomArrowPopupActivity extends Activity implements View.OnClic
                 });
                 break;
             case R.id.buttion3:
-                showTipPopupWindow(mButton3, new View.OnClickListener() {
+                mCurPopupWindow = showTipPopupWindow(mButton3, new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(getBaseContext(), "点击到弹窗内容", Toast.LENGTH_SHORT).show();
@@ -74,7 +68,7 @@ public class TopBottomArrowPopupActivity extends Activity implements View.OnClic
                 });
                 break;
             case R.id.buttion4:
-                showTipPopupWindow(mButton4, new View.OnClickListener() {
+                mCurPopupWindow = showTipPopupWindow(mButton4, new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(getBaseContext(), "点击到弹窗内容", Toast.LENGTH_SHORT).show();
@@ -90,7 +84,7 @@ public class TopBottomArrowPopupActivity extends Activity implements View.OnClic
                 });
                 break;
             case R.id.buttion6:
-                showTipPopupWindow(mButton6, new View.OnClickListener() {
+                mCurPopupWindow = showTipPopupWindow(mButton6, new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(getBaseContext(), "点击到弹窗内容", Toast.LENGTH_SHORT).show();
@@ -103,6 +97,9 @@ public class TopBottomArrowPopupActivity extends Activity implements View.OnClic
     public PopupWindow showTipPopupWindow(final View anchorView, final View.OnClickListener onClickListener) {
         final View contentView = LayoutInflater.from(anchorView.getContext()).inflate(R.layout.popuw_content_top_arrow_layout, null);
         contentView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        // 创建PopupWindow时候指定高宽时showAsDropDown能够自适应
+        // 如果设置为wrap_content,showAsDropDown会认为下面空间一直很充足（我以认为这个Google的bug）
+        // 备注如果PopupWindow里面有ListView,ScrollView时，一定要动态设置PopupWindow的大小
         final PopupWindow popupWindow = new PopupWindow(contentView,
                 contentView.getMeasuredWidth(), contentView.getMeasuredHeight(), false);
 
@@ -117,22 +114,32 @@ public class TopBottomArrowPopupActivity extends Activity implements View.OnClic
         contentView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
+                // 自动调整箭头的位置
                 autoAdjustArrowPos(popupWindow, contentView, anchorView);
                 contentView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
             }
         });
         // 如果不设置PopupWindow的背景，有些版本就会出现一个问题：无论是点击外部区域还是Back键都无法dismiss弹框
         popupWindow.setBackgroundDrawable(new ColorDrawable());
+
         // setOutsideTouchable设置生效的前提是setTouchable(true)和setFocusable(false)
         popupWindow.setOutsideTouchable(true);
-        popupWindow.setTouchable(true);             // 设置为true之后，PopupWindow才可以响应事件
-        popupWindow.setFocusable(false);            //
+
+        // 设置为true之后，PopupWindow内容区域 才可以响应点击事件
+        popupWindow.setTouchable(true);
+
+        // true时，点击返回键先消失 PopupWindow
+        // 但是设置为true时setOutsideTouchable，setTouchable方法就失效了（点击外部不消失，内容区域也不响应事件）
+        // false时PopupWindow不处理返回键
+        popupWindow.setFocusable(false);
         popupWindow.setTouchInterceptor(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                return false;
+                return false;   // 这里面拦截不到返回键
             }
         });
+        // 如果希望showAsDropDown方法能够在下面空间不足时自动在anchorView的上面弹出
+        // 必须在创建PopupWindow的时候指定高度，不能用wrap_content
         popupWindow.showAsDropDown(anchorView);
         return popupWindow;
     }
@@ -156,4 +163,12 @@ public class TopBottomArrowPopupActivity extends Activity implements View.OnClic
         downArrowParams.leftMargin = arrowLeftMargin;
     }
 
+    @Override
+    public void onBackPressed() {
+        if (mCurPopupWindow != null && mCurPopupWindow.isShowing()) {
+            mCurPopupWindow.dismiss();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
