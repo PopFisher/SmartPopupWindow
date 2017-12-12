5c4493ee21d4ef164a5e4ef6d419de63c43d0aa6
yuzhangsheng <774232122@qq.com>
2017-12-12 20:13:13 +0800

    增加解决横竖屏切换update方法失效的解决方案代码

--- app/src/main/java/popup/popfisher/com/smartpopupwindow/ScreenChangeUpdatePopupActivity.java
+++ app/src/main/java/popup/popfisher/com/smartpopupwindow/ScreenChangeUpdatePopupActivity.java
@@ -1,22 +1,24 @@
 package popup.popfisher.com.smartpopupwindow;
 
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.PopupWindow;
 
 public class ScreenChangeUpdatePopupActivity extends Activity {
     private Button mAnchorBtn;
     private PopupWindow mPopupWindow = null;
+    private int mCurOrientation = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,19 +35,39 @@ public class ScreenChangeUpdatePopupActivity extends Activity {
                 mPopupWindow.setOutsideTouchable(true);
                 mPopupWindow.setBackgroundDrawable(new ColorDrawable());
                 mPopupWindow.showAsDropDown(mAnchorBtn, 0, 0);
+                // showAsDropDown里面注册了一个OnScrollChangedListener,我们自己也注册一个OnScrollChangedListener
+                // 但是要在它的后面，这样系统回调的时候会先做完它的再做我们自己的，就可以用我们自己正确的值覆盖掉它的
+                // initViewListener();
             }
         });
     }
 
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        // 转屏时调用update方法更新位置，现象如下
+        // 转屏时直接调用update方法更新位置，现象如下
         // 1. 如果R.layout.activity_screen_change_update_popup中的ListView可见，则update无效
         // 2. 如果R.layout.activity_screen_change_update_popup中的ListView不可见，则update有效
-        final int typeScreen = newConfig.orientation;
-        if (typeScreen == ActivityInfo.SCREEN_ORIENTATION_USER || typeScreen == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+        mCurOrientation = newConfig.orientation;
+        // 如果要解决上面的问题就把下面这句话注释掉，并且打开注释掉的initViewListener
+        updatePopupPos();
+    }
+
+    private void initViewListener() {
+        mAnchorBtn.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                if (mPopupWindow == null || !mPopupWindow.isShowing()) {
+                    return;
+                }
+                updatePopupPos();
+            }
+        });
+    }
+
+    private void updatePopupPos() {
+        if (mCurOrientation == ActivityInfo.SCREEN_ORIENTATION_USER || mCurOrientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
             mPopupWindow.update(0, 0, -1, -1);
-        } else if (typeScreen == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+        } else if (mCurOrientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
             mPopupWindow.update(0, 800, -1, -1);
         }
     }
